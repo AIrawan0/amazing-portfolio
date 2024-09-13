bc7d22819dba5bd4cda13f9a44c72983de1039b7
AIrawan0 <airawan1035@gmail.com>
2024-09-13 16:51:33 +0700

    Initial commit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,26 @@
+<footer class=" py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center">
+    <p class="px-4 py-2 bg-whit text-slate-950 font-medium">
+        Connect with me &darr;
+    </p>
+    <div class="flex flex-col gap-4 items-center justify-center">
+        <p><b class=" pr-2">Email</b>airawan1035@gmail.com</p>
+        <p><b class=" pr-2">Github</b>
+            <a href="https://github.com/AIrawan0/" class=" text-violet-400" target="_blank">Asep Irawan
+                <sup>
+                    <span class=" text-xs scale-75 pl-0.5">
+                        <i class=" fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i>
+                    </span>
+                </sup>
+            </a>
+        </p>
+        <p><b class=" pr-2">LinkedIn</b>
+            <a href="https://www.linkedin.com/in/asepirawan/" class=" text-violet-400" target="_blank">Asep Irawan
+                <sup>
+                    <span class=" text-xs scale-75 pl-0.5">
+                        <i class=" fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i>
+                    </span>
+                </sup>
+            </a>
+        </p>
+    </div>
+</footer>
